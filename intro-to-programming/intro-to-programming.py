98cb15340112bcdd7eeb9ca47faf558ba306fdde
taiiisho <jjoly.dev@outlook.com>
2026-08-01 20:05:47 +0000

    String Types / 00:02:15:00 Session. Day 2

--- intro-to-programming/intro-to-programming.py
+++ intro-to-programming/intro-to-programming.py
@@ -11,7 +11,7 @@ print(365 * 24)  # There are 365 days in a year, 24 hours in a day
 name = input("What is your name?")
 print("Hello, " + name)
 
-name = input("what is your name?") # This is some practice
+name = input("what is your name?")  # This is some practice
 print(name)
 print(name)
 
@@ -34,10 +34,10 @@ print("Your email address:" + email)
 print("Your nickname:" + nickname)
 
 confirmation = input("Is this correct? YES / NO" " ")
-print("Okay, thank you for fufilling our request. " \
-"We will contact you within a 48 hour time-frame.")
+print("Okay, thank you for fufilling our request. "
+      "We will contact you within a 48 hour time-frame.")
 
-# Practice 
+# Practice
 The_1st_part = input(" ")
 The_2nd_part = input(" ")
 The_3rd_part = input(" ")
@@ -46,6 +46,81 @@ print(The_1st_part + "-" + The_2nd_part + "-" + The_3rd_part + "!")
 
 story_name = input("story name?")
 story_year = input("story year?")
-print(story_name + " is a valiant knight, born in the year " + story_year + 
-    ". One morning " + story_name + " woke up to an awful racket: a dragon was approaching the village. Only " +
-    story_name + " could save the village's residents.")
+print(story_name + " is a valiant knight, born in the year " + story_year +
+      ". One morning " + story_name + " woke up to an awful racket: a dragon was approaching the village. Only " +
+      story_name + " could save the village's residents.")
+
+# Day #2 variables
+given_name = "Paul"
+family_name = "Python"
+name = given_name + " " + family_name
+print(name)
+
+word = input("Please type in a word: ")
+print(word)
+word = input("And another word: ")
+print(word)
+word = "third"
+print(word)
+
+
+word = input("Please type in a word: ")
+print(word)
+
+word = word + "!!!"
+print(word)
+
+number1 = 100
+number2 = "100"
+
+print(number1 + number1)
+print(number2 + number2)
+
+result = 10 * 25
+print("The result is " + str(result))
+
+result = 10 * 25
+print("The result is", result)
+
+# f-strings
+result = 10 * 25
+print(f"The result is {result}")
+
+# All 3 Examples
+name_fstring = input("name")
+age_fstring = input("age")
+city_fstring = input("city")
+print(
+    f"Hi {name_fstring}, you are "
+    f"{age_fstring} years old. "
+    f"You live in {city_fstring}."
+)
+
+name_str = input("name")
+age_str = input("age")
+city_str = input("city")
+print(
+    "Hi" + " " + str(name_str) + ", " + "you are " 
+    + str(age_str) + ". " + "You live in" +
+    str(city_str) + "."
+)
+
+name = input("name")
+age = input("age")
+city = input("city")
+print("Hi", name, ", you are", age, "years old. You live in",
+        city, ".")
+
+uno = 97
+dos = 99
+tres = 100
+mean = (uno + dos + tres) / 3 
+print(f"Mean: {mean}")
+
+
+quiz1 = 90
+quiz2 = 100 
+quiz3 = 84
+quiz4 = 105
+mean = (quiz1 + quiz2 + quiz3 + quiz4) / 4 
+print(f"Mean: {mean}")
